66d15eb2cb0afcdf69829c08e3567655fa810100
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-05-07 06:34:31 +0200

    add form state management

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 
 import { validate } from '../../util/validators';
 import './Input.css';
@@ -29,8 +29,19 @@ const Input = (props) => {
     isValid: false,
   });
 
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid, onInput]);
+
   const changeHandler = (event) => {
-    dispatch({ type: 'CHANGE', val: event.target.value, validators: props.validators });
+    dispatch({
+      type: 'CHANGE',
+      val: event.target.value,
+      validators: props.validators,
+    });
   };
 
   const touchHandler = () => {
