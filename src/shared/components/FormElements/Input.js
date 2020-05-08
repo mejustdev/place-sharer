7e91cced106cf25b73da0aa2a1ad5d6b9d692917
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-05-08 03:09:16 +0200

    Adjusting the Input Component

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -9,44 +9,44 @@ const inputReducer = (state, action) => {
       return {
         ...state,
         value: action.val,
-        isValid: validate(action.val, action.validators),
+        isValid: validate(action.val, action.validators)
       };
     case 'TOUCH': {
       return {
         ...state,
-        isTouched: true,
-      };
+        isTouched: true
+      }
     }
     default:
       return state;
   }
 };
 
-const Input = (props) => {
+const Input = props => {
   const [inputState, dispatch] = useReducer(inputReducer, {
-    value: '',
+    value: props.value || '',
     isTouched: false,
-    isValid: false,
+    isValid: props.valid || false
   });
 
   const { id, onInput } = props;
   const { value, isValid } = inputState;
 
   useEffect(() => {
-    onInput(id, value, isValid);
+    onInput(id, value, isValid)
   }, [id, value, isValid, onInput]);
 
-  const changeHandler = (event) => {
+  const changeHandler = event => {
     dispatch({
       type: 'CHANGE',
       val: event.target.value,
-      validators: props.validators,
+      validators: props.validators
     });
   };
 
   const touchHandler = () => {
     dispatch({
-      type: 'TOUCH',
+      type: 'TOUCH'
     });
   };
 
@@ -72,9 +72,8 @@ const Input = (props) => {
 
   return (
     <div
-      className={`form-control ${
-        !inputState.isValid && inputState.isTouched && 'form-control--invalid'
-      }`}
+      className={`form-control ${!inputState.isValid && inputState.isTouched &&
+        'form-control--invalid'}`}
     >
       <label htmlFor={props.id}>{props.label}</label>
       {element}
