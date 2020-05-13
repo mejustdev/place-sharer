91db324567d9640d5081001e85fbef3633c2461a
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-05-13 02:19:11 +0200

    Adding Patch Routes to Update Places

--- Backend/controllers/places-controllers.js
+++ Backend/controllers/places-controllers.js
@@ -9,17 +9,17 @@ const DUMMY_PLACES = [
     description: 'One of the most famous sky scrapers in the world!',
     location: {
       lat: 40.7484474,
-      lng: -73.9871516,
+      lng: -73.9871516
     },
     address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1',
-  },
+    creator: 'u1'
+  }
 ];
 
 const getPlaceById = (req, res, next) => {
   const placeId = req.params.pid; // { pid: 'p1' }
 
-  const place = DUMMY_PLACES.find((p) => {
+  const place = DUMMY_PLACES.find(p => {
     return p.id === placeId;
   });
 
@@ -36,12 +36,14 @@ const getPlaceById = (req, res, next) => {
 const getPlaceByUserId = (req, res, next) => {
   const userId = req.params.uid;
 
-  const place = DUMMY_PLACES.find((p) => {
+  const place = DUMMY_PLACES.find(p => {
     return p.creator === userId;
   });
 
   if (!place) {
-    return next(new HttpError('Could not find a place for the provided user id.', 404));
+    return next(
+      new HttpError('Could not find a place for the provided user id.', 404)
+    );
   }
 
   res.json({ place });
@@ -56,7 +58,7 @@ const createPlace = (req, res, next) => {
     description,
     location: coordinates,
     address,
-    creator,
+    creator
   };
 
   DUMMY_PLACES.push(createdPlace); //unshift(createdPlace)
@@ -64,6 +66,24 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({place: updatedPlace});
+};
+
+const deletePlace = (req, res, next) => {};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
