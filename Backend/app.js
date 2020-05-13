14c0554d0e73fea9e14045ea3354a9302c729232
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-05-13 02:08:57 +0200

    Handling Errors for Unsupported Routes

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
@@ -9,12 +10,17 @@ app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes); // => /api/places...
 
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+});
+
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500);
-  res.json({ message: error.message || 'An unknown error occurred!' });
+  res.status(error.code || 500)
+  res.json({message: error.message || 'An unknown error occurred!'});
 });
 
 app.listen(5000);
