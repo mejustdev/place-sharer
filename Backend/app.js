43cd206ff91143e222ef696ebc806d7e8c2e964d
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-05-18 04:24:53 +0200

    add mongoose connection

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -21,8 +22,23 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500)
-  res.json({message: error.message || 'An unknown error occurred!'});
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
-app.listen(5000);
+const connectUrl =
+  'mongodb+srv://mergorgec:BNfrL4MxVyuWUslo@place-sharer-wmlr6.mongodb.net/test?retryWrites=true&w=majority';
+const connectConfig = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+};
+mongoose
+  .connect(connectUrl, connectConfig)
+  .then(() => {
+    console.log('+++ Database connected! +++');
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
