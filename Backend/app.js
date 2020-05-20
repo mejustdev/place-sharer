930355e0f4e7a7a3323c9368d9bd5160be878528
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-05-20 04:56:43 +0200

    Handling CORS Errors

--- Backend/app.js
+++ Backend/app.js
@@ -10,6 +10,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
@@ -27,7 +38,7 @@ app.use((error, req, res, next) => {
 });
 
 const connectUrl =
-  'mongodb+srv://mergorgec:xxx@place-sharer-wmlr6.mongodb.net/mern?retryWrites=true&w=majority';
+  'mongodb+srv://mergorgec:KowhRhMoabodwItc@place-sharer-wmlr6.mongodb.net/mern?retryWrites=true&w=majority';
 const connectConfig = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
