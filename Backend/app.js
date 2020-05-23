6d3514ae9b8c8b3d97a37b7f5683bf959bab2b1a
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-05-23 04:11:09 +0200

    Serving Images Statically

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,8 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const fs = require('fs');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -10,6 +11,8 @@ const HttpError = require('./models/http-error');
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
