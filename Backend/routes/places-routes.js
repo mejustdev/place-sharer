ad6d01d9a251cc917af8c273381ee4edc189abc3
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-05-25 00:57:47 +0200

    Backend Route Protection with Auth Middleware

--- Backend/routes/places-routes.js
+++ Backend/routes/places-routes.js
@@ -3,6 +3,7 @@ const { check } = require('express-validator');
 
 const placesControllers = require('../controllers/places-controllers');
 const fileUpload = require('../middleware/file-upload');
+const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
 
@@ -10,30 +11,23 @@ router.get('/:pid', placesControllers.getPlaceById);
 
 router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
+router.use(checkAuth);
+
 router.post(
   '/',
   fileUpload.single('image'),
   [
-    check('title')
-      .not()
-      .isEmpty(),
+    check('title').not().isEmpty(),
     check('description').isLength({ min: 5 }),
-    check('address')
-      .not()
-      .isEmpty()
+    check('address').not().isEmpty(),
   ],
-  placesControllers.createPlace
+  placesControllers.createPlace,
 );
 
 router.patch(
   '/:pid',
-  [
-    check('title')
-      .not()
-      .isEmpty(),
-    check('description').isLength({ min: 5 })
-  ],
-  placesControllers.updatePlace
+  [check('title').not().isEmpty(), check('description').isLength({ min: 5 })],
+  placesControllers.updatePlace,
 );
 
 router.delete('/:pid', placesControllers.deletePlace);
